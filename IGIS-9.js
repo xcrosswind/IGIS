ae680669bbf7b0595fd2a594ea717b2c29851431
Tobias Brühlmeier <tobias.bruehlmeier@crosswind.ch>
2015-03-31 16:25:10 +0200

    zusammengeführt

--- IGIS-9.js
+++ IGIS-9.js
@@ -15,12 +15,15 @@ require([
   , "esri/geometry/Circle"
   , "esri/graphic"
   , "esri/InfoTemplate"
+  , "esri/dijit/Popup" 
+  , "esri/dijit/PopupTemplate"
   , "esri/symbols/SimpleMarkerSymbol"
   , "esri/symbols/SimpleLineSymbol"
   , "esri/symbols/SimpleFillSymbol"
   , "esri/renderers/SimpleRenderer"
   , "esri/dijit/Search"
   , "esri/dijit/BasemapGallery"
+  , "esri/units"
   
   , "esri/config"
   
@@ -35,7 +38,10 @@ require([
   
   , "dojo/parser"
   , "dojo/dom"
+  , "dojo/dom-construct"
   , "dojo/on"
+  
+  , "dojo/dom-class"
   
   , "dijit/layout/BorderContainer"
   , "dijit/layout/ContentPane"
@@ -52,12 +58,15 @@ require([
   , Circle
   , Graphic
   , InfoTemplate
+  , Popup
+  , PopupTemplate
   , SimpleMarkerSymbol
   , SimpleLineSymbol
   , SimpleFillSymbol
   , SimpleRenderer
   , Search  
   , BasemapGallery
+  , Units
   , esriConfig
   , TooltipDialog
   , esriLang
@@ -68,16 +77,56 @@ require([
   , arcgisUtils
   , parser
   , dom
+  , domConstruct
   , on
+  , domClass
 
 ) {
   'use strict';
   
+ 
+  // create popup
+  var fill = new SimpleFillSymbol("solid", null, new Color("#A4CE67"));
+  var popup = new Popup({
+    fillSymbol: fill,
+    titleInBody: false, 
+    visibleWhenEmpty: false, // warum funktioniert das nicht? Ich will eigentlich, dass ein leeres Popup nicht gezeigt wird
+    hideDelay: 0
+  }, domConstruct.create("div"));
+
+  var host = "http://sdshgweb01:9010"; // muss wahrscheinlich noch irgendwie dynamisch gesetzt werden, um zwischen extern und intern unterscheiden zu können    
+  var infoDescription = "<b>STAT_CODE: </b>${STAT_CODE}<br>"
+      + "<b>STAT_ID: </b>{STAT_ID}<br>"
+      + "<b>STAT_NAME: </b>{STAT_NAME}<br>"
+      + "<b>IS_GSM: </b>{IS_GSM}<br>"
+      + "<b>IS_DCS: </b>{IS_DCS}<br>"
+      + "<b>IS_UMTS: </b>{IS_UMTS}<br>"
+      + "<b>IS_LTE: </b>{IS_LTE}<br>"
+      + "<b>IS_OUTD: </b>{IS_OUTD}<br>"
+      + "<b>IS_INHOUS: </b>{IS_INHOUS}<br>"
+      + "<b>IS_TUNNEL: </b>{IS_TUNNEL}<br>"
+      + "<a href= '" + host + "/Indigo/Station/Detail/{STAT_ID}' target='_blank'>Link auf Indigo Details</a><br>"
+  var template = new PopupTemplate({
+    title: "STAT_NAME: {STAT_NAME}",
+    description: infoDescription  ,
+  /*  fieldInfos: [{ //define field infos so we can specify an alias
+      fieldName: "STAT_ID",
+      label: "STAT_ID"
+    }, {
+      fieldName: "IS_LTE",
+      label: "IS_LTE"   
+    }] */
+  });
+
+
+
   // create map
   map = new Map("map", {
     basemap : "streets",
-    slider : true
+    slider : true,
+    infoWindow: popup
   });
+  
   
   // add basemap gallery, in this case we'll display maps from ArcGIS.com including bing maps
   var basemapGallery = new BasemapGallery({
@@ -97,16 +146,18 @@ require([
     }, "search");
   search.startup();
   // -- end search field
-                             
+  
+        
   // create featureLayer
   var mapServiceUrl = "https://stgeo01/arcgis/rest/services/IGIS/IGIS_Sites_Userselection/MapServer/0";
-  var featureLayer = new FeatureLayer(
-    mapServiceUrl,
-    { infoTemplate : new InfoTemplate("Block: ${BLOCK}", "${*}"),
-      outFields : [ "USER_ID", "ACTIVE_GUID", "STAT_ID", "STAT_CODE",
+  
+  var featureLayer = new FeatureLayer(mapServiceUrl, {
+    infoTemplate: template,
+    outFields : [ "USER_ID", "ACTIVE_GUID", "STAT_ID", "STAT_CODE",
           "STAT_NAME", "LON", "LAT", "IS_GSM", "IS_DCS", "IS_UMTS", "IS_LTE", "IS_OUTD", "IS_INHOUS", "IS_TUNNEL" ]
   });
   // -- end featureLayer
+  
   
   // query data based on Map Service URL parameters
   var queryTask = new QueryTask(mapServiceUrl);
@@ -117,7 +168,6 @@ require([
   var url = urlUtils.urlToObject(document.location.href);
   
   query.where = "user_id = " + url.query['user_id'] + " AND active_guid = '" + url.query['active_guid'] + "'";
-  //query.where = "USER_ID = " + url.query.user_id + " AND ACTIVCE_GUID = '" + url.query.active_guid + "'";  // diese Version funktioniert nicht. Warum?
                             
   // only load required features, selected by user_id and GUID
   featureLayer.setDefinitionExpression(query.where);
@@ -125,10 +175,6 @@ require([
   
   
     function showResults(featureSet) {
-    featureLayer.selectFeatures(query,
-    FeatureLayer.SELECTION_NEW, function(featureSet) {
-    });
-    
     // pan and zoom to objects (median of coordinates, to omit points (Dachsen Thörishausen))
     function median(values) {
       values.sort( function(a,b) {return a - b;} );
@@ -161,84 +207,19 @@ require([
           new Color([ 255, 0, 0, 1 ]));
     featureLayer.setSelectionSymbol(selectedSymbol);
         
-    var notselectedSymbol = new SimpleMarkerSymbol(
+    var nonselectedSymbol = new SimpleMarkerSymbol(
       SimpleMarkerSymbol.STYLE_CIRCLE, 7,
       new SimpleLineSymbol(SimpleLineSymbol.STYLE_NULL,
         new Color([ 247, 34, 101, 0.9 ]), 1),
           new Color([ 255, 0, 0, 1 ]));
-    featureLayer.setRenderer(new SimpleRenderer(notselectedSymbol));
+    featureLayer.setRenderer(new SimpleRenderer(nonselectedSymbol));
     
     map.addLayer(featureLayer);
   }
-  
-  
-  // --- Mouseover Window
-  map.infoWindow.resize(245,125); 
-  
-  var dialog = new TooltipDialog({
-    id: "tooltipDialog",
-    style: "position: absolute; width: 250px; font: normal normal normal 10pt Helvetica;z-index:100"
-  });
-  dialog.startup();
-  
-  //close the dialog when the mouse leaves the highlight graphic
-  map.on("load", function(){
-    map.graphics.enableMouseEvents();
-    map.graphics.on("mouse-out", closeDialog);             
-  });
 
 
-                     
-  //listen for when the onMouseOver event fires on the countiesGraphicsLayer
-  //when fired, create a new graphic with the geometry from the event.graphic and add it to the maps graphics layer
-  
-  // -- DIESE VARIANTE FUNKTIONIERT MIT MOUSEOVER
-  // leider ist damit der Link nicht erreichbar, hier wäre eine andere Lösung zu suchen
-  featureLayer.on("mouse-over", showTooltip);
-  
-  function showTooltip(evt) {
-    var host = "http://sdshgweb01:9010"; // muss wahrscheinlich noch irgendwie dynamisch gesetzt werden, um zwischen extern und intern unterscheiden zu können    
-    var t = "<b>STAT_CODE: </b>${STAT_CODE}<br>"
-      + "<b>STAT_NAME: </b>${STAT_NAME}<br>"
-      + "<b>IS_GSM: </b>${IS_GSM}<br>"
-      + "<b>IS_DCS: </b>${IS_DCS}<br>"
-      + "<b>IS_UMTS: </b>${IS_UMTS}<br>"
-      + "<b>IS_LTE: </b>${IS_LTE}<br>"
-      + "<b>IS_OUTD: </b>${IS_OUTD}<br>"
-      + "<b>IS_INHOUS: </b>${IS_INHOUS}<br>"
-      + "<b>IS_TUNNEL: </b>${IS_TUNNEL}<br>"
-      + "<a href= '" + host + "/Indigo/Station/Detail/${STAT_ID}' target='_blank'>Link auf Indigo Details</a><br>";
-    
-  // set mouseover window
-  var content = esriLang.substitute(evt.graphic.attributes,t);         
-  dialog.setContent(content);
-  
-  domStyle.set(dialog.domNode, "opacity", 0.75);
-  dijitPopup.open({
-    popup: dialog, 
-    x: evt.pageX,
-    y: evt.pageY
-    });
-  }
-  
-  featureLayer.on("mouse-out", closeDialog); 
-  
-  function closeDialog() {
-    map.graphics.clear();
-    dijitPopup.close(dialog);
-  }
-  // -- MOUSEOVER
-  
-  
-  
-  
-  
-  
-  
-  var unselectSymbol = new SimpleMarkerSymbol().setSize(0);
-  featureLayer.setRenderer(new SimpleRenderer(unselectSymbol));
-  // Fenster verschwindet beim Mouseout noch nicht
-  // -- END Mouseover Window  
+
+
 
   //when the map is clicked create a buffer around the click point of the specified distance.        
   // Selection Circle
@@ -253,13 +234,16 @@ require([
 
   // select objects inside circle
   map.on("click", function(evt){
+    
+    
     circle = new Circle({
       center: evt.mapPoint,
       geodesic: true,
-      radius: 100 // Default Value: Meters
+      radius: 100, // Default Value: Meters
+      radiusUnit: Units.METERS
     });
     map.graphics.clear();
-    map.infoWindow.hide();
+    map.infoWindow.show();
     var graphic = new Graphic(circle, circleSymb);
     map.graphics.add(graphic);
   
@@ -282,10 +266,11 @@ require([
         inBuffer.push(feature.attributes[featureLayer.objectIdField]);
       }
     }
-    query.objectIds = inBuffer;
-    //use a fast objectIds selection query (should not need to go to the server)
-    featureLayer.selectFeatures(query, FeatureLayer.SELECTION_NEW, function(results){
+    var query = new Query();
+    query.objectIds = inBuffer; //use a fast objectIds selection query (should not need to go to the server)
+    featureLayer.selectFeatures(query, FeatureLayer.SELECTION_NEW, function(results){ 
+      console.log("Anzahl Objekte: " + featureLayer.getSelectedFeatures().length); // access to selected features via featureLayer selection
+      console.log(featureLayer.getSelectedFeatures()[0].attributes);
     });
-  //  console.log(featureLayer.getSelectedFeatures()); // access to selected features via featureLayer selection
   }
 });
