ffe70abf71c318053643995fc6aceb6a2b1d8ce9
ckiefer <christoph.kiefer@gmail.com>
2015-04-13 11:59:47 +0200

    Revert "Modified IGIS-X files"
    This reverts commit 68907954394f9e1b1dd0d083fb96d33741f23335.

--- IGIS-X.php
+++ IGIS-X.php
@@ -3,22 +3,16 @@ require_once "vendor/autoload.php";
 
 set_include_path ( '../../../igis' );
 require ('IGIS-settings.php');
-require ('Encryption_Module.php');
-
-$crypt = new encryption_class ();
 
 $config = new \Doctrine\DBAL\Configuration ();
 $connectionParams = array (
 		'dbname' => $db_name,
 		'user' => $db_user,
-		// 'password' => $db_pw,
-		'password' => $crypt->decrypt ( $db_secret, $db_pw ),
+		'password' => $db_pw,
 		'host' => $db_hostname,
 		'port' => $db_port,
 		'driver' => $db_driver 
 );
-// DEBUG ? error_log ( $connectionParams ["password"] . "\n", 3, $GLOBALS ["igis_access_log"] ) : null;
-
 // connect to database
 $db = \Doctrine\DBAL\DriverManager::getConnection ( $connectionParams, $config );
 
@@ -51,7 +45,7 @@ if ($actionType == 1) {
 function insertIntoSelex() {
 	$queryBuilder = $GLOBALS ["db"]->createQueryBuilder ();
 	
-	$queryBuilder->insert ( $GLOBALS ["db_schema"] . "." . $GLOBALS ["db_x_selex"] ); // TODO: check this for Orcale!
+	$queryBuilder->insert ( $GLOBALS ["db_schema"] . "." . $GLOBALS ["db_x_selex"] );
 	$queryBuilder->values ( array (
 			'user_id' => '?',
 			'src_id' => '?',
@@ -78,7 +72,7 @@ function getSelexId() {
 	$queryBuilder = $GLOBALS ["db"]->createQueryBuilder ();
 	
 	$queryBuilder->select ( 'selex_id' );
-	$queryBuilder->from ( $GLOBALS ["db_schema"] . "." . $GLOBALS ["db_x_selex"] ); // TODO: check this for Orcale!
+	$queryBuilder->from ( $GLOBALS ["db_schema"] . "." . $GLOBALS ["db_x_selex"] );
 	$queryBuilder->where ( 'user_id = ?' );
 	$queryBuilder->andwhere ( 'src_id = ?' );
 	$queryBuilder->andwhere ( 'time_stamp = ?' );
@@ -106,7 +100,7 @@ function addSelexData($selex_id, $objects) {
 	$queryBuilder = $GLOBALS ["db"]->createQueryBuilder ();
 	
 	foreach ( $objects as $o ) {
-		$queryBuilder->insert ( $GLOBALS ["db_schema"] . "." . $GLOBALS ["db_x_selexdata"] ); // TODO: check this for Orcale!
+		$queryBuilder->insert ( $GLOBALS ["db_schema"] . "." . $GLOBALS ["db_x_selexdata"] );
 		$queryBuilder->values ( array (
 				'selex_id' => '?',
 				'obj_id' => '?' 
@@ -124,7 +118,7 @@ function addSelexData($selex_id, $objects) {
 function delSelexData($selex_id) {
 	$queryBuilder = $GLOBALS ["db"]->createQueryBuilder ();
 	
-	$queryBuilder->delete ( $GLOBALS ["db_schema"] . "." . $GLOBALS ["db_x_selexdata"] ); // TODO: check this for Orcale!
+	$queryBuilder->delete ( $GLOBALS ["db_schema"] . "." . $GLOBALS ["db_x_selexdata"] );
 	$queryBuilder->where ( 'selex_id = ?' );
 	$queryBuilder->setParameter ( 0, $selex_id );
 	
