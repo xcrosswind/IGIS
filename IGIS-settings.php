68907954394f9e1b1dd0d083fb96d33741f23335
ckiefer <christoph.kiefer@gmail.com>
2015-04-13 11:29:08 +0200

    Modified IGIS-X files

--- IGIS-settings.php
+++ IGIS-settings.php
@@ -7,16 +7,24 @@ $db_schema = 'bto_shogun';
 $db_x_selex = 'x_selex';
 $db_x_selexdata = 'x_selexdata';
 $db_user = 'postgres';
-$db_pw = 'postgres';
+$db_pw_plain = 'postgres';
+$db_pw = 'lG^tG?s+';
+
 $db_driver = 'pdo_pgsql';
 
+$db_secret = 'This is my secret key; with symbols (@$^*&<?>/!#_+), cool eh?!!! :)';
+
 // Indigo DB Development
-// $db_hostname = 'tbd';
-// $db_port = 'tbd';
-// $db_name = 'tbd';
-// $db_schema = 'tbd';
-// $db_user = 'tbd';
+
+// $db_hostname = 'sTgpdb01';
+// $db_ip = '10.239.181.131';
+// $db_port = '2149';
+// $db_name = 'D148SHG';
+// $db_schema = 'tbd'; // TODO: check if necessary for Oracle!
+// $db_user = 'APP_ARCGIS';
+// $db_pw_plain = 'tbd';
 // $db_pw = 'tbd';
+
 // $db_driver = 'pdo_oci';
 
 // logging
